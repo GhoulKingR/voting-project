360b63fcd01d46d6391f09838b32db442208dcc5
Chigozie Oduah <oduahchigozie46@gmail.com>
2025-03-08 21:10:38 +0100

    completed dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,57 @@
+"use client";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
+import styled from "styled-components";
+
+
+function submitFactory(router: AppRouterInstance) {
+  return function(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    router.push('/dashboard');
+  }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  return (
+    <Main>
+      <Form className="rounded-[22px] px-[63px] pt-[15px] pb-[133px] w-[90%] max-w-[646px]" onSubmit={submitFactory(router)}>
+        <Image src="/images/c4bcd117d567f60f81f40bf701cbc96f.png" alt="Logo" width={99.14} height={103} className="mx-auto mb-[12px]" />
+        <h1 className="text-[40px] leading-[48px] font-semibold text-center mb-[44px]">Student login</h1>
+
+        <label htmlFor="matric-input" className="font-medium text-[14px] leading-[20px] block">Matric/Application Number</label>
+        <Input type="text" placeholder="Enter Matric/Application number" name="username" id="matric-input" className="block w-full bg-white rounded-[6px] py-[8px] px-[12px] mb-[40px]"/>
+
+        <label htmlFor="password-input" className="font-medium text-[14px] leading-[20px] block">Password</label>
+        <Input type="password" id="password-input" name="password" placeholder="Enter your password" className="block w-full bg-white rounded-[6px] py-[8px] px-[12px] mb-[40px]"/>
+
+        <div className="text-right text-[#0FACFF] font-medium text-[14px] leading-[24px] mb-[24px]">Forgot password</div>
+
+        <button type="submit" className="w-full py-[12px] bg-[#0FACFF] text-white rounded-[8px] font-semibold text-[16px] leading-[24px]">Log In</button>
+      </Form>
+    </Main>
   );
 }
+
+const Main = styled.main`
+  background-image: url('/images/6e2529d06601b42a402d5b2e4ce4ac9e.jpeg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  background: rgba(255, 255, 255, .9);
+`;
+
+const Input = styled.input`
+  border: 1px solid rgba(0, 0, 0, .1);
+`;
